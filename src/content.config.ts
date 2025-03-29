8a6e08c562fbbf00792e30cc808b81353bac2fcc
Stel Clementine <dev@stelclementine.com>
2025-03-29 01:32:39 -0700

    Coerce dates in markdown frontmatter

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,8 +5,8 @@ const postsCollection = defineCollection({
   loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./src/content/posts" }),
   schema: z.object({
     title: z.string(),
-    published: z.date(),
-    // updated: z.date().optional(),
+    published: z.coerce.date(),
+    // updated: z.coerce.date().optional(),
     draft: z.boolean().optional().default(false),
     description: z.string().optional().default(''),
     // image: z.string().optional().default(''),
