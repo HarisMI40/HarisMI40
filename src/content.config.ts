14d45858e34bc939d2ce2466346a9fc4668c97a3
Stel Clementine <dev@stelclementine.com>
2025-03-29 01:13:57 -0700

    Add loader to "posts" content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 const postsCollection = defineCollection({
+  loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./src/content/posts" }),
   schema: z.object({
     title: z.string(),
     published: z.date(),
