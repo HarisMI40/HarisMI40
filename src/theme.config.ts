9de59a4d2e63062de1f28e8b75cca867a6d5ef15
Stel Clementine <dev@stelclementine.com>
2025-03-31 22:05:52 -0700

    Add default theme config

---
+++ src/theme.config.ts
@@ -0,0 +1,12 @@
+import type { ThemeConfig } from '@types'
+import '@fontsource-variable/jetbrains-mono'
+
+const config: ThemeConfig = {
+  theme: 'nord',
+  font: 'JetBrains Mono Variable',
+  accent: 'terminal.ansiBrightCyan',
+  foreground: "editor.foreground",
+  background: "editor.background"
+}
+
+export default config
