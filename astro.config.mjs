c2f73aa80095dd10ec9043dca88ee494b905a0ef
Stel Clementine <dev@stelclementine.com>
2025-03-31 22:23:58 -0700

    Rename theme.config.ts to site.config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import expressiveCode from "astro-expressive-code";
-import themeConfig from './src/theme.config'
+import siteConfig from './src/site.config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -44,7 +44,7 @@ export default defineConfig({
   integrations: [
     sitemap(),
     expressiveCode({
-      themes: [themeConfig.theme],
+      themes: [siteConfig.theme.name],
       defaultProps: {
         wrap: false,
       },
