f58175bab36d079b664aebc3e2e92c9700957f01
Stel Clementine <dev@stelclementine.com>
2025-04-01 22:35:51 -0700

    Add util functions to simplify getting colors from themes

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,31 @@
+import type { ExpressiveCodeTheme } from "astro-expressive-code";
+
 export function dateString(date: Date) {
   return date.toISOString().split('T')[0];
+}
+
+export function flattenThemeColors(theme: ExpressiveCodeTheme): { [key: string]: string } {
+  const scopedThemeSettings = theme.settings.reduce(
+    (acc, item) => {
+      const { scope, settings } = item
+      const { foreground } = settings
+      console.log(scope)
+      if (scope && foreground) {
+        for (const s of scope) {
+          acc[`${s}.foreground`] = foreground
+        }
+      }
+      return acc
+    },
+    {} as { [key: string]: string },
+  )
+  return { ...theme.colors, ...scopedThemeSettings }
+}
+
+export function headingColor(level: number, themeColors: { [key: string]: string }): string | undefined {
+  return (
+    themeColors[`heading.${level}.markdown entity.name`] ||
+    themeColors['markup.heading.foreground'] ||
+    undefined
+  )
 }
