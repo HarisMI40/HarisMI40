b5729a96fa0db7974c382772df8eb608e6560286
Stel Clementine <dev@stelclementine.com>
2025-04-02 13:36:46 -0700

    Change heading anchor links to look like markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import expressiveCode from "astro-expressive-code";
 import siteConfig from './src/site.config'
+import { h } from 'hastscript'
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,24 +17,19 @@ export default defineConfig({
       [
         rehypeAutolinkHeadings,
         {
-          behavior: 'append',
+          behavior: 'prepend',
           properties: {
-            className: ['anchor'],
+            className: ['heading-anchor', 'inline-block', 'text-accent/90', 'mr-2'],
           },
-          content: {
-            type: 'element',
-            tagName: 'span',
-            properties: {
-              className: ['anchor-icon', 'text-accent/90', 'ml-2', 'hover:underline'],
-              'data-pagefind-ignore': true,
-            },
-            children: [
-              {
-                type: 'text',
-                value: '#',
-              },
-            ],
-          },
+          content: (node) => {
+            let x
+            try {
+              x = parseInt(node.tagName.charAt(1)) - 1
+            } catch (e) {
+              x = 1
+            }
+            return h('span', { class: 'anchor-text', 'data-pagefind-ignore': true }, ["#".repeat(x)])
+          }
         },
       ],
     ],
