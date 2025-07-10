76ff09e1ddd28703b91be7d3eabd23cf11194650
Stel Clementine <dev@stelclementine.com>
2025-07-10 13:21:41 -0500

    Update example site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,11 +4,16 @@ import '@fontsource-variable/jetbrains-mono'
 const config: SiteConfig = {
   title: "Terminal",
   theme: {
-    name: 'github-dark',
+    // name: 'kanagawa-wave',
+    // name: 'catppuccin-frappe',
+    name: 'everforest-dark',
+    // name: 'synthwave-84',
+    // name: 'rose-pine-dawn',
+    // name: 'github-light-high-contrast',
+    // name: 'tokyo-night',
+    // name: 'nord',
+    // name: 'one-dark-pro',
     font: 'JetBrains Mono Variable',
-    accent: 'terminal.ansiBrightGreen',
-    foreground: "editor.foreground",
-    background: "editor.background"
   }
 }
 
