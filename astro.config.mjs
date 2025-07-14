73fe4c4148aedba16a1c499affc3625b1657c9a3
Stel Clementine <dev@stelclementine.com>
2025-07-13 21:02:28 -0500

    Rework heading anchor links

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ import expressiveCode from "astro-expressive-code";
 import siteConfig from './src/site.config'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import icon from 'astro-icon';
-import { createHeadingAnchor, remarkDescription, remarkReadingTime, rehypeTitleFigure } from './src/settings-utils';
+import { remarkDescription, remarkReadingTime, rehypeTitleFigure } from './src/settings-utils';
+import {fromHtmlIsomorphic} from 'hast-util-from-html-isomorphic'
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,11 +26,16 @@ export default defineConfig({
       [
         rehypeAutolinkHeadings,
         {
-          behavior: 'prepend',
+          behavior: 'append',
           properties: {
-            className: ['heading-anchor', 'inline-block', 'text-accent/90', 'mr-2'],
+            className: ['heading-anchor'],
           },
-          content: createHeadingAnchor,
+          content: (
+            fromHtmlIsomorphic(
+              '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link-icon lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>',
+              {fragment: true}
+            ).children
+          )
         },
       ],
       rehypeTitleFigure,
--- src/settings-utils.ts
+++ src/settings-utils.ts
@@ -4,16 +4,6 @@ import { h } from "hastscript";
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 
-export function createHeadingAnchor(node: hast.Element): hast.Element {
-  let x
-  try {
-    x = parseInt(node.tagName.charAt(1))
-  } catch (e) {
-    x = 1
-  }
-  return h('span', { class: 'anchor-text', 'data-pagefind-ignore': true }, ["#".repeat(x)])
-}
-
 export const remarkDescription: RemarkPlugin = (options?: { maxChars?: number }) => {
   const maxChars = options && options.maxChars || 200
   return function(tree, { data }) {
